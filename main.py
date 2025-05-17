762d5d42b0ba0f3cdc1748115bb7cd36a27abf30
sitscholl <tscholl.simon@gmail.com>
2025-05-17 15:11:36 +0200

    moved parameters to yaml file

--- main.py
+++ main.py
@@ -1,42 +1,34 @@
 import numpy as np
 import pandas as pd
-import src
+import matplotlib.pyplot as plt
+from jinja2 import Environment, FileSystemLoader
+
 from tempfile import TemporaryDirectory
 from pathlib import Path
 import datetime
-import matplotlib.pyplot as plt
-from jinja2 import Environment, FileSystemLoader
 import logging
 import logging.config
+import yaml
 
-logging.config.fileConfig(".config/logging.conf", disable_existing_loggers=False)
+import src
 
-logging.basicConfig(level=logging.DEBUG)
+## Open Config 
+with open("config.yaml", 'r') as f:
+    config = yaml.safe_load(f)
+
+## Setup logging
+logging.config.dictConfig(config['logging'])
 logger = logging.getLogger(__name__)
 
-#################
-# Parameters
-#################
-r = 100
-p = (642202.50, 5163856.06)
-crs = 25832
-height = 7
-slope = 15
-aspect = 270
-efficiency = (0.15, 0.2)
-system_loss = (0.75, 0.85)
-price = 45 #ct/kWh
-area_optim = None
-consumption_tbl = 'data/power_consumption.xlsx'
-optim_file = 'data/optim/optim_result_2025_02_22_1443.csv'
-#################
+logger.info('Starting program at %s', datetime.datetime.now().strftime('%Y%m%d %H:%M'))
 
-logger.info('Starting program at %s', datetime.datetime.now().strftime('%Y_%m_%d_%H%M'))
-dem_p = f"data/dem{r}m.tif"
+dem_resolution = config.get("dem_resolution", 100)
+dem_p = f"data/dem{dem_resolution}m.tif"
 
 ##Parameter optimization
-if optim_file is None:
+if config.get('optim_file') is None:
     logger.info('Optimizing parameters')
+
     import arcpy
     files = list(Path('data/optim').glob('*.csv'))
     observed_srad = src.load_monthly_radiation(files)
@@ -57,8 +49,9 @@ if optim_file is None:
     )
     t_opt, d_opt = src.get_optim_values(optim_tbl, metric = 'rmse')
 else:
-    logger.info(f'Using optimized parameters from {optim_file}')
-    t_opt, d_opt = src.get_optim_values(pd.read_csv(optim_file), metric="rmse")
+    logger.info(f'Using optimized parameters from {config.get("optim_file")}')
+    ##TODO: get monthly optimized values and not singe value for whole year
+    t_opt, d_opt = src.get_optim_values(pd.read_csv(config.get('optim_file')), metric="rmse")
 # optim_lines(optim_tbl, observed_srad, '19300MS', t_opt, d_opt)
 
 
@@ -66,12 +59,12 @@ out_dir = Path(TemporaryDirectory().name)
 out_dir.mkdir(exist_ok = True, parents = True)
 insolation = src.feature_insolation(
     dem=dem_p,
-    features=[p],
-    crs=crs,
+    features=[config['location']],
+    crs=config['crs'],
     out_dir=out_dir,
-    feature_offset=height,
-    feature_slope=slope,
-    feature_aspect=aspect,
+    feature_offset=config['panels']['south']['height'],
+    feature_slope=config['panels']['south']['slope'],
+    feature_aspect=config['panels']['south']['aspect'],
     transmittivity=t_opt,
     diffuse_proportion=d_opt,
     interval_unit="DAY",
@@ -80,7 +73,7 @@ insolation = src.feature_insolation(
 
 insolation_mon = insolation.set_index('date').resample('MS')[['global_ave']].sum()
 
-power_cns = pd.read_excel(consumption_tbl, usecols = ['date', 'consumption'])
+power_cns = pd.read_excel(config.get('consumption_tbl'), usecols = ['date', 'consumption'])
 power_cns['date'] = pd.to_datetime(power_cns['date'], format = '%Y-%m-%d')
 power_cns.set_index('date', inplace = True)
 
@@ -118,6 +111,7 @@ for area in [5, 10, 15, 20, 25, 30]:
 ax.legend()
 ax.set_ylim(0, 750)
 
+area_optim = config.get('area_optim')
 if area_optim is None:
     area_optim = [i for i in dict_diff.keys() if np.abs(dict_diff[i]) == min(np.abs(list(dict_diff.values())))][0]
 
@@ -129,22 +123,22 @@ environment = Environment(loader=FileSystemLoader("templates/"))
 template = environment.get_template("report.html")
 content = template.render(
     rep_date=datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
-    dem_resolution = r,
-    point_coordinates = p,
-    panel_height = height,
-    slope = slope,
-    aspect = aspect,
-    efficiency = efficiency,
-    system_loss = system_loss,
-    price_per_kwh = price,
+    dem_resolution = dem_resolution,
+    point_coordinates = config['location'],
+    panel_height = config['panels']['south']['height'],
+    slope = config['panels']['south']['slope'],
+    aspect = config['panels']['south']['aspect'],
+    efficiency = config['panels']['south']['efficiency'],
+    system_loss = config['panels']['south']['system_loss'],
+    price_per_kwh = config.get('price', 45),
 
     monthly_energy_chart = src.encode_plot(fig),
 
     area_optim = area_optim,
-    kWp = f"{area_optim * efficiency[0]:.2f} - {area_optim * efficiency[1]:.2f}",
+    kWp = f"{area_optim * config['panels']['south']['efficiency'][0]:.2f} - {area_optim * config['panels']['south']['efficiency'][1]:.2f}",
     total_annual_energy = en_tot_ann,
     total_annual_consumption = energy_tbl["consumption"].sum(),
-    avoided_costs = ((en_tot_ann * price) / 100).round(1),
+    avoided_costs = ((en_tot_ann * config.get('price', 45)) / 100).round(1),
 
     monthly_energy = monthly_energy,
     monthly_consumption = monthly_consumption,
