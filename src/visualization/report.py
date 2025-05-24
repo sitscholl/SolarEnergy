994724525552188af1ce6cedc66d17e83b407c61
sitscholl <tscholl.simon@gmail.com>
2025-05-24 18:02:58 +0200

    include pandera data validation

---
+++ src/visualization/report.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandera.pandas as pa
+
+import logging
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
+
+## Dataframe validation
+class ConsumptionSchema(pa.DataFrameModel):
+    date: datetime = pa.Field()
+    consumption: float = pa.Field(ge=0)
+
+class IncomingRadiationSchema(pa.DataFrameModel):
+    date: datetime = pa.Field()
+    srad: float = pa.Field(ge=0)
+
+
+class Report:
+
+    def __init__(self, srad: pd.Series, consumption: pd.Series | None = None):
+        IncomingRadiationSchema.validate(srad)
+        self.srad = srad.set_index('date')
+
+        if consumption is not None:
+            ConsumptionSchema.validate(consumption)
+            consumption = consumption.set_index('date')
+
+            consumption_freq = consumption.index.dt.infer_freq()
+            production_freq = srad.index.dt.infer_freq()
+
+            if consumption_freq != production_freq:
+                logger.warning(f"Datetime frequency in production table does not match frequency in consumption table. Got {production_freq} vs {consumption_freq}. Resampling consumption data")
+                consumption = consumption.resample(production_freq).mean()
+
+        self.consumption = consumption
+
+    def solar_energy_to_electric_energy(
+        srad, efficiency=0.15, system_loss=0.8, area=None, kWp=None
+    ):
+        """
+        Convert solar radiation input into electricity output.
+
+        Parameters
+        ----------
+        srad : float
+            Total solar radiation (kWh/m²) for the location and time period.
+        efficiency : float, optional
+            Efficiency of the solar panels. Defaults to 0.15.
+            Commercial solar panels typically convert 15–22% of sunlight into electricity
+        system_loss : float, optional
+            Factor in losses from inverters (3–10%), temperature (5–15%), dust/shading (2–10%), and wiring (1–3%).
+            Defaults to 0.8 (20% total losses).
+        area : float, optional
+            Area covered by solar panels (m²). Must be provided if `kWp` is not.
+        kWp : float, optional
+            System size (kWp). Must be provided if `area` is not.
+
+        Returns
+        -------
+        out : float
+            The total electricity output (kWh) of the system.
+
+        Notes
+        -----
+        To calculate the area needed for a system, use the formula:
+
+        Area (m²) = System Size (kWp) / (Irradiance (1 kW/m²) × Efficiency)
+
+        This requires a known efficiency and a known system size. This formula
+        explains much area is needed under perfect lab conditions to get that the given kWp.
+        The 1 comes from the standard 1kW/m² used in lab conditions to assess kWp
+
+        To calculate the total electricity output the following formula is used:
+
+        Total Electricity Output (kWh) = Solar Radiation (kWh/m²) × Area (m²) × Efficiency × System Loss Factor
+        """
+
+        if area is None and kWp is None:
+            raise ValueError("Either area or kWp must be provided")
+        elif area is not None and kWp is not None:
+            raise ValueError("Only one of area or kWp must be provided")
+
+        if kWp is not None:
+            area = kWp / (1 * efficiency)
+
+        return srad * area * efficiency * system_loss
+
+    def calculate_energy_efficiency(self, freq = 'M'):
+        pass
+
+    def calculate_energy_surplus(self, freq = 'M'):
+        pass
+
+    def calculate_energy_savings(self, freq = 'M'):
+        pass
+
+    def plot(self):
+
+        fig, ax = plt.subplots(figsize = (12, 6))
+
+        if self.consumption is not None:
+            ax.plot(self.consumption, label = "Consumption", color = 'black')
+        ax.plot(self.production, label = "Production", color = 'red')
+
+        ax.legend()
+        ax.set_ylim(0, 750)
+
+    def generate_report(self, template):
+        pass
